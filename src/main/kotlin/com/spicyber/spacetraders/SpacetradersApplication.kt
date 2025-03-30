6667d111902d041d4a057a3078ec48173e18c84d
kathrinthmsn <kathrin.thomsen@gmx.net>
2025-03-30 13:22:16 +0200

    Initial commit

---
+++ src/main/kotlin/com/spicyber/spacetraders/SpacetradersApplication.kt
@@ -0,0 +1,11 @@
+package com.spicyber.spacetraders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpacetradersApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpacetradersApplication>(*args)
+}
---
+++ src/test/kotlin/com/spicyber/spacetraders/SpacetradersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spicyber.spacetraders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpacetradersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
